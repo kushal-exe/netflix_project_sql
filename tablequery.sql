60d7f8181ea3e1dff988cc51f8acd8ec8646246b
melon_khushal <159040519+kushal-exe@users.noreply.github.com>
2024-10-05 00:20:49 +0530

    Add files via upload

---
+++ tablequery.sql
@@ -0,0 +1,15 @@
+CREATE TABLE netflix_3 (
+    show_id CHARACTER VARYING(10),
+    type CHARACTER VARYING(10),
+    title CHARACTER VARYING(150),
+    director CHARACTER VARYING(250),
+    casts CHARACTER VARYING(1000),
+    country CHARACTER VARYING(150),
+    date_added CHARACTER VARYING(50),
+    release_year INTEGER,
+	rating CHARACTER VARYING(10),
+    duration CHARACTER VARYING(15),s
+    listed_in CHARACTER VARYING(100),
+    description CHARACTER VARYING(300)
+);
+ 
